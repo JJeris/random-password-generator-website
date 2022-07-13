518906c57ca32a47c1a91b0533391d8666002091
JJ <60788469+JJeris@users.noreply.github.com>
2022-07-13 21:28:51 +0300

    Finished css styling for index.html.

--- scripts/password_generator.js
+++ scripts/password_generator.js
@@ -1,6 +1,22 @@
 // Assets - lists of characters that will be used in the generation of password.
 
-// var uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// var lowercase_letters = "abcdefghijklmnopqrstuvwxyz"
-// var numbers = "1234567890"
-// var symbols = "`~!@#$%^&*()_+-={|[]\:;<>?,./:"
+
+
+
+
+
+var uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var symbols = "`~!@#$%^&*()_+-={|[]\:;<>?,./:";
+
+// Takes value from the form.
+function Add_length() {
+
+}
+
+var password_length = 10;
+function Generate_password (password_length) {
+    var password_output = "";
+    
+}
