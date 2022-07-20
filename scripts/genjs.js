50b85fabf78d88f8814e403f58672058edb1cc23
JJ <60788469+JJeris@users.noreply.github.com>
2022-07-21 00:28:09 +0300

    Semi functional prototype. YAY :D

--- scripts/genjs.js
+++ scripts/genjs.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 //Assets used to gen. password (i.e. letters, numbers, symbols)
 var txt0 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var txt1 = "abcdefghijklmnopqrstuvwxyz"
@@ -50,3 +55,50 @@ function b(a){
     }
 }
 addlen()
+
+// Assets - lists of characters that will be used in the generation of password.
+
+// var uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// var lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
+// var numbers = "1234567890";
+// var symbols = "`~!@#$%^&*()_+-={|[]\:;<>?,./:";
+
+// Takes value from the form.
+// function Add_length() {
+//     var password_length = prompt("Add length:")
+//     while (password_length<=0){
+//         var password_length = prompt("Invalid input. Add length:")
+//     }
+//     return password_length;
+// }
+
+// var button = document.getElementById("generate-password-button"),
+// value =  button.form.valueId.value;
+// button.onclick = Generate_password();
+
+// function Generate_password () {
+//     var password_length = document.getElementById("password-length").value;
+//     console.log(password_length);
+
+//     var password_output = "";
+//     for (i = 0; i <= password_length; i++){
+//         var x = Math.floor(Math.random() * 3);
+//         if (x==0){
+//             var x0 = Math.floor(Math.random() * uppercase_letters.length)
+//             password_output = password_output+uppercase_letters[x0]
+//         }
+//         if (x==1){
+//             var x1 = Math.floor(Math.random() * lowercase_letters.length)
+//             password_output = password_output+lowercase_letters[x1]
+//         }
+//         if (x==2){
+//             var x2 = Math.floor(Math.random() * numbers.length)
+//             password_output = password_output+numbers[x2]
+//         }
+//         if (x==3){
+//             var x3 = Math.floor(Math.random() * symbols.length)
+//             password_output = password_output+symbols[x3]
+//         }
+//     }
+//     document.getElementById("generated-password-paragraph").innerHTML = password_output;
+// }
--- scripts/password_generator.js
+++ scripts/password_generator.js
@@ -1,22 +1,81 @@
-// Assets - lists of characters that will be used in the generation of password.
-
-
 var uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "1234567890";
 var symbols = "`~!@#$%^&*()_+-={|[]\:;<>?,./:";
 
-// Takes value from the form.
-function Add_length() {
-    var password_length = prompt("Add length:")
-    while (password_length<=0){
-        var password_length = prompt("Invalid input. Add length:")
-    }
-    return password_length;
-}
+var form = document.getElementById('form')
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    var password_length = document.getElementById('password-length').value;
 
-var password_length = 10;
-function Generate_password (password_length) {
+    console.log(password_length);
+
     var password_output = "";
+    for (i = 0; i <= password_length; i++){
+        var x = Math.floor(Math.random() * 3);
+        if (x==0){
+            var x0 = Math.floor(Math.random() * uppercase_letters.length)
+            password_output = password_output+uppercase_letters[x0]
+        }
+        if (x==1){
+            var x1 = Math.floor(Math.random() * lowercase_letters.length)
+            password_output = password_output+lowercase_letters[x1]
+        }
+        if (x==2){
+            var x2 = Math.floor(Math.random() * numbers.length)
+            password_output = password_output+numbers[x2]
+        }
+        if (x==3){
+            var x3 = Math.floor(Math.random() * symbols.length)
+            password_output = password_output+symbols[x3]
+        }
+    }
+    console.log(password_output);
+    document.getElementById("generated-password-paragraph").innerHTML = password_output;
+    // DIFF FONT PLEASE
+})
+// if(form){
+//     form.addEventListener('submit', function(event){
+//         event.preventDefault(); // Doesnt default form autosubmitting.
+//         var password_length = document.getElementById('password-length').value;
+//         console.log(password_length);
+//     })
+    
+// }
 
-}
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     function Generate_password () {
+//         var password_length = document.getElementById('password-length');
+//         form.addEventListener('generate-password-button', (e) => {
+
+//             var uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//             var lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
+//             var numbers = "1234567890";
+//             var symbols = "`~!@#$%^&*()_+-={|[]\:;<>?,./:";
+
+//             var password_output = "";
+//             for (i = 0; i <= password_length; i++){
+//                 var x = Math.floor(Math.random() * 3);
+//                 if (x==0){
+//                     var x0 = Math.floor(Math.random() * uppercase_letters.length)
+//                     password_output = password_output+uppercase_letters[x0]
+//                 }
+//                 if (x==1){
+//                     var x1 = Math.floor(Math.random() * lowercase_letters.length)
+//                     password_output = password_output+lowercase_letters[x1]
+//                 }
+//                 if (x==2){
+//                     var x2 = Math.floor(Math.random() * numbers.length)
+//                     password_output = password_output+numbers[x2]
+//                 }
+//                 if (x==3){
+//                     var x3 = Math.floor(Math.random() * symbols.length)
+//                     password_output = password_output+symbols[x3]
+//                 }
+//             }
+//             console.log(password_output);
+//             // document.getElementById("generated-password-paragraph").innerHTML = password_output;
+//         })
+//     }
+//     document.getElementById("generate-password-button").addEventListener('click', Generate_password);
+// })
