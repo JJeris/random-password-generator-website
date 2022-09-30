c7802e9e6bed528fa7093662bd6fb0bbe5612933
Johans Justs Eris <johans.eris@r6vsk.lv>
2022-10-01 00:24:23 +0300

    Finish project

--- scripts/password_generator.js
+++ scripts/password_generator.js
@@ -1,89 +1,43 @@
-// Change let names to the standard.
 const UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "1234567890";
+const LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz";
+const NUMBERS = "1234567890";
 // const symbols = "`~!@#$%^&*()_+-={}[]|\;:',<.>/?";
-const symbols = "`~!@#$%^&*()_+-={}[];:',.?";
+// const SYMBOLS = "`~!@#$%^&*()_+-={}[];:',.?";
+// const SYMBOLS = "`~!@#$%^*()_+={}[];:',.?";
+const SYMBOLS = "`~!@#$%^*()_+={}[];:',.";
 
 let form = document.getElementById('form')
 form.addEventListener('submit', function(event){
     event.preventDefault();
     let password_length = document.getElementById('password-length').value;
 
-    console.log(password_length);
+    // console.log(password_length);
 
     let password_output = "";
     for (i = 0; i <= password_length-1; i++){
         let x = Math.floor(Math.random() *4);
-        console.log(x);
+        // console.log(x);
         if (x==0){
             let x0 = Math.floor(Math.random() * UPPERCASE_LETTERS.length)
             password_output = password_output+UPPERCASE_LETTERS[x0]
         }
         if (x==1){
-            let x1 = Math.floor(Math.random() * lowercase_letters.length)
-            password_output = password_output+lowercase_letters[x1]
+            let x1 = Math.floor(Math.random() * LOWERCASE_LETTERS.length)
+            password_output = password_output+LOWERCASE_LETTERS[x1]
         }
         if (x==2){
-            let x2 = Math.floor(Math.random() * numbers.length)
-            password_output = password_output+numbers[x2]
+            let x2 = Math.floor(Math.random() * NUMBERS.length)
+            password_output = password_output+NUMBERS[x2]
         }
         if (x==3){
-            let x3 = Math.floor(Math.random() * symbols.length)
-            password_output = password_output+symbols[x3]
+            let x3 = Math.floor(Math.random() * SYMBOLS.length)
+            password_output = password_output+SYMBOLS[x3]
         }
     }
-    console.log(password_output);
+    // console.log(password_output);
 
     // Add functionality that segments the output into sub paragraph tags, centered and textalign centered on the webpage.
     document.getElementById("generated-password-paragraph").innerHTML = password_output;
     
 })
 
-
-
-// if(form){
-//     form.addEventListener('submit', function(event){
-//         event.preventDefault(); // Doesnt default form autosubmitting.
-//         var password_length = document.getElementById('password-length').value;
-//         console.log(password_length);
-//     })
-    
-// }
-
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     function Generate_password () {
-//         var password_length = document.getElementById('password-length');
-//         form.addEventListener('generate-password-button', (e) => {
-
-//             var uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-//             var lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
-//             var numbers = "1234567890";
-//             var symbols = "`~!@#$%^&*()_+-={|[]\:;<>?,./:";
-
-//             var password_output = "";
-//             for (i = 0; i <= password_length; i++){
-//                 var x = Math.floor(Math.random() * 3);
-//                 if (x==0){
-//                     var x0 = Math.floor(Math.random() * uppercase_letters.length)
-//                     password_output = password_output+uppercase_letters[x0]
-//                 }
-//                 if (x==1){
-//                     var x1 = Math.floor(Math.random() * lowercase_letters.length)
-//                     password_output = password_output+lowercase_letters[x1]
-//                 }
-//                 if (x==2){
-//                     var x2 = Math.floor(Math.random() * numbers.length)
-//                     password_output = password_output+numbers[x2]
-//                 }
-//                 if (x==3){
-//                     var x3 = Math.floor(Math.random() * symbols.length)
-//                     password_output = password_output+symbols[x3]
-//                 }
-//             }
-//             console.log(password_output);
-//             // document.getElementById("generated-password-paragraph").innerHTML = password_output;
-//         })
-//     }
-//     document.getElementById("generate-password-button").addEventListener('click', Generate_password);
-// })
