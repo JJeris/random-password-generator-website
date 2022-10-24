accf3356c9bd848d309f3ed0cbcc31791b87a819
Johans Justs Eris <johans.eris@r6vsk.lv>
2022-10-24 10:54:28 +0300

    Feature update Added a copy to clipboard button with JS. Added width to the nav bottom border. Added animation to buttons. Footer imgs decrease opacity.

---
+++ scripts/copy_password.js
@@ -0,0 +1,15 @@
+function copyText(htmlElement) {       
+    if (!htmlElement) {
+        return;
+    }
+    let elementText = htmlElement.innerText;
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('value', elementText);
+    document.body.appendChild(inputElement);
+    inputElement.select();
+    document.execCommand('copy');
+    inputElement.parentNode.removeChild(inputElement);
+  }
+  document.querySelector("#copy-text-btn").onclick = function () {
+    copyText(document.querySelector("#generated-password-paragraph"));
+  }
